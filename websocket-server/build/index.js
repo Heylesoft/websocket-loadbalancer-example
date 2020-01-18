5e97b4d32cce8e03d3cb1902b142335ee8cd3eb5
Jefferson Ortiz <jortizq@belatrixsf.com>
2020-01-17 19:23:54 -0500

    GSS-8 #comment Create default Typescript project

---
+++ websocket-server/build/index.js
@@ -0,0 +1,7 @@
+class App {
+    constructor() {
+        console.log("Hello App Socket");
+    }
+}
+const app = new App();
+//# sourceMappingURL=index.js.map
---
+++ websocket-server/src/index.ts
@@ -0,0 +1,7 @@
+class App {
+    constructor(){
+        console.log("Hello App Socket");
+    }
+}
+
+const app: App = new App();
